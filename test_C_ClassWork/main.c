bd97a634c5940a96ed0fb4fe5b6399a4a67652d4
ciec6574 <2283199586@qq.com>
2018-11-16 20:28:15 +0800

    Initial Commit

---
+++ test_C_ClassWork/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test_C_ClassWork
+//
+//  Created by s20171106574 on 2018/11/16.
+//  Copyright © 2018 s20171106574. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
